e8cb426105f0f1d7478a9e3d31a7ef2ae67b27d8
hariquasar <harrythedoctorofficial@gmail.com>
2024-01-17 22:18:06 +0800

    update

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
   new THREE.MeshBasicMaterial({
-    map: new THREE.TextureLoader().load('./img/globe.png')
+    map: new THREE.TextureLoader().load('/img/globe_01.jpg')
   })
 )
 
